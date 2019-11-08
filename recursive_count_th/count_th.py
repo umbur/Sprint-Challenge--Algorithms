cc543bef74d70d9242475579fca74737b6876638
umbur <you@example.com>
2019-11-08 10:59:51 -0700

    Initial commit

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -2,6 +2,7 @@
 Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
+Initial commit
 '''
 def count_th(word):
     
